6158460ac0e0743cc66132311ec2921c53f8b92c
Kurt Stephens <ks.github@kurtstephens.com>
2012-03-14 11:32:21 -0500

    Avoid require twice by path, not name.

--- lib/require_faster.rb
+++ lib/require_faster.rb
@@ -1,15 +1,30 @@
-$stderr.puts "loading #{__FILE__}"
+# $stderr.puts "loading #{__FILE__}"
+require 'thread'
+
 module RequireFaster
   def self.activate!
     PathWatcher.activate!
     self.activate_Kernel!
     self
   end
+
+  LOADED = { }
+  LOADED_MUTEX = Mutex.new
+
   def self.activate_Kernel!
     Kernel.module_eval do
       def require_faster name
         path = ::RequireFaster::Cache.instance.find_in_search_path(name)
-        require_without_require_faster path
+        unless path
+          raise LoadError, "no such file to load -- #{name}"
+        end
+        do_load = 
+        LOADED_MUTEX.synchronize do
+          unless LOADED[path]
+            LOADED[path] = name
+          end
+        end
+        do_load and require_without_require_faster(path) # => true
       end
       alias :require_without_require_faster :require
       alias :require :require_faster
